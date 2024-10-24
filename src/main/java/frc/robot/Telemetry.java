efe80c76f7e35cf80ff13d1672c121fa5de7002a
jratke <jratke@cps.edu>
2024-10-24 15:52:25 -0500

    Replace NetwrokTables telemetey with AdvantageKit

--- src/main/java/frc/robot/Telemetry.java
+++ src/main/java/frc/robot/Telemetry.java
@@ -1,17 +1,12 @@
 package frc.robot;
 
+import org.littletonrobotics.junction.Logger;
+
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain.SwerveDriveState;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.networktables.DoubleArrayPublisher;
-import edu.wpi.first.networktables.DoublePublisher;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.StringPublisher;
-import edu.wpi.first.networktables.StructArrayPublisher;
 
 public class Telemetry {
     /**
@@ -19,27 +14,6 @@ public class Telemetry {
      * 
      */
     public Telemetry() {}
-
-    /* What to publish over networktables for telemetry */
-    private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
-
-    /* Robot pose for field positioning */
-    private final NetworkTable table = inst.getTable("Pose");
-    private final DoubleArrayPublisher fieldPub = table.getDoubleArrayTopic("robotPose").publish();
-    private final StringPublisher fieldTypePub = table.getStringTopic(".type").publish();
-
-    /* Robot speeds for general checking */
-    private final NetworkTable driveStats = inst.getTable("Drive");
-    private final DoublePublisher velocityX = driveStats.getDoubleTopic("Velocity X").publish();
-    private final DoublePublisher velocityY = driveStats.getDoubleTopic("Velocity Y").publish();
-    private final DoublePublisher speed = driveStats.getDoubleTopic("Speed").publish();
-    private final DoublePublisher odomPeriod = driveStats.getDoubleTopic("Odometry Period").publish();
-
-    /* Measured and Desired module states */
-    StructArrayPublisher<SwerveModuleState> moduleStates = NetworkTableInstance.getDefault()
-        .getStructArrayTopic("AdvantageScope/ModuleStates", SwerveModuleState.struct).publish();
-    StructArrayPublisher<SwerveModuleState> desiredStates = NetworkTableInstance.getDefault()
-        .getStructArrayTopic("AdvantageScope/DesiredStates", SwerveModuleState.struct).publish();
 
     /* Keep a reference of the last pose to calculate the speeds */
     private Pose2d m_lastPose = new Pose2d();
@@ -49,12 +23,7 @@ public class Telemetry {
     public void telemeterize(SwerveDriveState state) {
         /* Telemeterize the pose */
         Pose2d pose = state.Pose;
-        fieldTypePub.set("Field2d");
-        fieldPub.set(new double[] {
-            pose.getX(),
-            pose.getY(),
-            pose.getRotation().getRadians()
-        });
+        Logger.recordOutput("Drive/Pose", pose);
 
         /* Telemeterize the robot's general speeds */
         double currentTime = Utils.getCurrentTimeSeconds();
@@ -65,13 +34,11 @@ public class Telemetry {
 
         Translation2d velocities = distanceDiff.div(diffTime);
 
-        speed.set(velocities.getNorm());
-        velocityX.set(velocities.getX());
-        velocityY.set(velocities.getY());
-        odomPeriod.set(state.OdometryPeriod);
+        Logger.recordOutput("Drive/Velocities", velocities);
+        Logger.recordOutput("Drive/OdometryPeriod", state.OdometryPeriod);
 
         /* Telemeterize the module's states */
-        moduleStates.set(state.ModuleStates);
-        desiredStates.set(state.ModuleTargets);
+        Logger.recordOutput("Drive/ModuleStates", state.ModuleStates);
+        Logger.recordOutput("Drive/DesiredStates", state.ModuleTargets);
     }
 }
