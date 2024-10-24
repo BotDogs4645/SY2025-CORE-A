63778f3f71a36351afdb0b3c76a97bd140798a2f
jratke <jratke@cps.edu>
2024-10-24 12:00:58 -0500

    Implement AdvantageKit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,19 +4,58 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  public final PowerDistribution pdh = new PowerDistribution(1, ModuleType.kRev); //Required for AKit current logging
   
   private final PDHTelemetry currentLogger = new PDHTelemetry();
 
   @Override
   public void robotInit() {
+    Logger.recordMetadata("ProjectName", "SY2025-mk4n");
+
+    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+    Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+    Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+    Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+    switch (BuildConstants.DIRTY) {
+        case 0:
+            Logger.recordMetadata("GitDirty", "Clean");
+            break;
+        case 1:
+            Logger.recordMetadata("GitDirty", "Dirty");
+            break;
+        default:
+            Logger.recordMetadata("GitDirty", "Error");
+            break;
+    }
+
+    if (isReal()) {
+        Logger.addDataReceiver(new WPILOGWriter()); // Log to a USB stick ("/U/logs")
+        Logger.addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+    } else {
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+        Logger.setReplaySource(new WPILOGReader(logPath)); // Read replay log
+        Logger.addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+    }
+
+    Logger.start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+
     m_robotContainer = new RobotContainer();
   }
 
