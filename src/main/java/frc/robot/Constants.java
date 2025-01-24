2cc327e025d3031a4a97e1a8c98cb556d63378a0
jratke <jratke@cps.edu>
2025-01-24 11:00:24 -0600

    feat(vision): add limelight localization

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,34 @@
+package frc.robot;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+
+public class Constants {
+    public class VisionConstants {
+        public static String[] limelightNames = {"limelight"};
+
+        // Basic filtering thresholds
+        public static double maxAmbiguity = 0.3;
+        public static double maxZError = 0.75;
+
+        // Ignore MT2 readings when robot is rotationg too fast
+        public static double maxAngluarVelocity = 2 * Math.PI; // Rad/s
+
+        public static AprilTagFieldLayout aprilTagLayout = 
+            AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+
+        // Standard deviation baselines, for 1 meter distance and 1 tag
+        // (Adjusted automatically based on distance and # of tags)
+        public static double linearStdDevBaseline = 0.02; // Meters
+        public static double angularStdDevBaseline = 0.06; // Radians
+
+        // Multipliers to apply for MegaTag 2 observations
+        public static double linearStdDevMegatag2Factor = 0.5; // More stable than full 3D solve
+        public static double angularStdDevMegatag2Factor = 
+            Double.POSITIVE_INFINITY; // No rotation data available
+
+        // Standard deviation multipliers for each camera
+        // (Adjust to trust some cameras more than others)
+        public static double[] limelightStdDevFactors = {1.0};
+    }
+}
