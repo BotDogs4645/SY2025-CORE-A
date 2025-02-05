9a85bb0626bf172d4533403302b18a3b6dd92993
Aidan <117467689+bsverdin@users.noreply.github.com>
2025-02-05 16:37:20 -0600

    chore: added comments for clarification

--- src/main/java/frc/robot/subsystems/DeepClimb.java
+++ src/main/java/frc/robot/subsystems/DeepClimb.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -7,6 +8,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class DeepClimb extends SubsystemBase {
     private final TalonFX climbMotor;
     private final TalonFX chuteMotor;
+    //this is to use the absolute encoder mode on the through bore encoders.
+    //We should be using the absolute mode since the climbing motor won't rotate more than once.
+    //To use it as an Absolute Encoder however, we do have to connect it to the DIO poet in the RoboRIO. https://www.chiefdelphi.com/t/rev-encoder-absolute-mode-code-examples/425761
     private final DigitalInput encoderInput;
     private final DutyCycleEncoder climbEncoder; 
     public DeepClimb(){
