50457884661c18cc69641a0df206afde366b6aeb
Aidan <117467689+bsverdin@users.noreply.github.com>
2025-02-05 16:29:08 -0600

    feat: climb method

---
+++ src/main/java/frc/robot/subsystems/DeepClimb.java
@@ -0,0 +1,26 @@
+package frc.robot.subsystems;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DeepClimb extends SubsystemBase {
+    private final TalonFX climbMotor;
+    private final TalonFX chuteMotor;
+    private final DigitalInput encoderInput;
+    private final DutyCycleEncoder climbEncoder; 
+    public DeepClimb(){
+        climbMotor = new TalonFX(0);
+        chuteMotor = new TalonFX(0);
+        encoderInput = new DigitalInput(0);
+        climbEncoder = new DutyCycleEncoder(encoderInput);
+    }
+    public void climb (double climbSpeed, double chuteSpeed){
+        while (chuteMotor.getPosition().getValueAsDouble() < 103.0 ){ //the value is in angles
+            chuteMotor.set(chuteSpeed);
+        }
+        while (climbEncoder.get() < 0.625){ //Need to test this value (the value is in rotations)
+            climbMotor.set(climbSpeed);
+        }
+    }
+}
