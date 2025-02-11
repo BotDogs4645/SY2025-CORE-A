62a99aeef6732d5d846aaaa9795901c3e0f5cfe2
JackHughess <jack.hughes1452@gmail.com>
2025-02-11 16:13:05 -0600

    refactor

--- src/main/java/frc/robot/commands/CommandBuilder.java
+++ src/main/java/frc/robot/commands/CommandBuilder.java
@@ -1,10 +1,21 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.Endeffector;
 
 public class CommandBuilder {
-    public static Command outputCoral(Endeffector endeffector, Elevator elevator) {
-        
+    public class EndEffector {
+
+        public Command outputCoral(Endeffector endEffector, int level) {
+            //move elevator and stuff
+
+            return Commands.run(
+
+                endEffector.setPivotPosition(level);
+
+
+            )
+        }
     }
 }
