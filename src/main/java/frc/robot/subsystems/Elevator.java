19f7872afe5368966ac7dd3becd142270f6be1e1
Zach Buchar <56662213+zachb1807@users.noreply.github.com>
2025-02-10 16:54:01 -0800

    feat(elevator): basic pid to move to position

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.ControlRequest;
 import com.ctre.phoenix6.controls.NeutralOut;
@@ -17,14 +18,17 @@ import frc.robot.Constants;
 
 public class Elevator extends SubsystemBase{
     private final TalonFX leftMotor;
-    private final TalonFX rightMotor;
+    // private final TalonFX rightMotor;
     private final DigitalInput lowerLimitSwitch;
     private boolean reachedBottom;
     // private final TalonFXConfiguration elevatorConfiguration;
 
     public Elevator() {
         leftMotor = new TalonFX(Constants.ElevatorConstants.leftMotorCANId);
-        rightMotor = new TalonFX(Constants.ElevatorConstants.rightMotorCANId);
+        var slot0Configs = new Slot0Configs();
+        slot0Configs.kP = 0.05;
+        leftMotor.getConfigurator().apply(slot0Configs);
+        // rightMotor = new TalonFX(Constants.ElevatorConstants.rightMotorCANId);
         lowerLimitSwitch = new DigitalInput(Constants.ElevatorConstants.lowerLimitSwitchDIOPort);
         reachedBottom = false;
 
@@ -33,17 +37,17 @@ public class Elevator extends SubsystemBase{
         leftMotor.getConfigurator().apply(currentConfigs);
 
         currentConfigs.Inverted = InvertedValue.Clockwise_Positive;
-        rightMotor.getConfigurator().apply(currentConfigs);
+        // rightMotor.getConfigurator().apply(currentConfigs);
     }
 
     public void setSpeed(double val) {
-        rightMotor.set(val);
+        // rightMotor.set(val);
         leftMotor.set(val);
     }
 
     public void stop() {
         leftMotor.set(0);
-        rightMotor.set(0);
+        // rightMotor.set(0);
     }
     
     public boolean getLimitSwitch() {
@@ -59,7 +63,7 @@ public class Elevator extends SubsystemBase{
 
     public void resetEncoders() {
         leftMotor.setPosition(0);
-        rightMotor.setPosition(0);
+        // rightMotor.setPosition(0);
     }
 
     public void setControl(ControlRequest control) {
@@ -68,11 +72,11 @@ public class Elevator extends SubsystemBase{
 
     public void enableBrakemode() {
         leftMotor.setNeutralMode(NeutralModeValue.Brake);
-        rightMotor.setNeutralMode(NeutralModeValue.Brake);
+        // rightMotor.setNeutralMode(NeutralModeValue.Brake);
     }
     public void enableCoastMode() {
         leftMotor.setNeutralMode(NeutralModeValue.Coast);
-        rightMotor.setNeutralMode(NeutralModeValue.Coast);
+        // rightMotor.setNeutralMode(NeutralModeValue.Coast);
     }
 
 
