d4a56690bb9c14fae8b9578ca73e6cb26696c9c3
bsverdin <bsverdin@cps.edu>
2025-02-12 15:33:37 -0600

    chore: accounting for gearbox

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -62,6 +63,7 @@ public class RobotContainer {
     JoystickButton stowButton = new JoystickButton(testController, 2);
     JoystickButton climbButton = new JoystickButton(testController, 3);
     private final DeepClimb deepClimb = new DeepClimb();
+
 
   public RobotContainer() {
     autoChooser.addCmd("First Path", this::firstPath);
