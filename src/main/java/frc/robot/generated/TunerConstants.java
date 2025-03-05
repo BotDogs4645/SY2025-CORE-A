3f440e974818e8be5561504c9c005e1288699d94
jratke <jratke@cps.edu>
2025-03-05 00:07:01 -0600

    temp fix: swap swerve module order

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -222,9 +222,15 @@ public class TunerConstants {
          * Creates a CommandSwerveDrivetrain instance.
          * This should only be called once in your robot program,.
          */
+        //public static CommandSwerveDrivetrain createDrivetrain() {
+        //        return new CommandSwerveDrivetrain(
+        //                        DrivetrainConstants, FrontLeft, FrontRight, BackLeft, BackRight);
+        //}
+        // IMPORTANT: THIS IS VERY TEMPORARY, PLEASE RECONFIGURE SWERVE
+        // change swerve module order to correct for rotation
         public static CommandSwerveDrivetrain createDrivetrain() {
                 return new CommandSwerveDrivetrain(
-                                DrivetrainConstants, FrontLeft, FrontRight, BackLeft, BackRight);
+                                DrivetrainConstants, BackRight, BackLeft, FrontRight, FrontLeft);
         }
 
         /**
