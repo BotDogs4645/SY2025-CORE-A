ed169ed2d2cc9e078e6141b5d39046bc15bbdfea
zachb1807 <56662213+zachb1807@users.noreply.github.com>
2025-03-27 09:37:55 -0500

    Last commit for event

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -316,8 +316,16 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             ), 
             Constants.PathPlannerConstants.config, 
             () -> {
+                // Boolean supplier that controls when the path will be mirrored for the red alliance
+                // This will flip the path being followed to the red side of the field.
+                // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+  
+                var alliance = DriverStation.getAlliance();
+                if (alliance.isPresent()) {
+                  return alliance.get() == DriverStation.Alliance.Red && DriverStation.isAutonomous();
+                }
                 return false;
-            }, 
+              },
             this
         );
 
