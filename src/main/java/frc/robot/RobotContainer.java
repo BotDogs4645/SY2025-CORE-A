57e91b33af8f251d5757d533d1dfb3b29eb1f00a
zachb1807 <56662213+zachb1807@users.noreply.github.com>
2025-03-27 12:08:41 -0500

    Update at 'Thu Mar 27 12:08:41 CDT 2025'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,6 +21,7 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.MechanismPosition;
@@ -89,6 +90,7 @@ public class RobotContainer {
 
         SmartDashboard.putData("Auto Chooser", autoChooser.getSendableChooser());
         SmartDashboard.putData("Field", drivetrain.getField());
+        SmartDashboard.putData(CommandScheduler.getInstance());
         
     }
 
