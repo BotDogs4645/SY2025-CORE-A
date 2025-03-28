7223001b4a363722b5d0a1b02cde39173f174917
Camden Rush <64759244+camden-git@users.noreply.github.com>
2025-03-28 09:50:16 -0500

    Update at 'Fri Mar 28 09:50:16 CDT 2025'

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -48,7 +48,7 @@ public class Elevator extends SubsystemBase {
         var leftConfig = config.withHardwareLimitSwitch(
             new HardwareLimitSwitchConfigs()
                 .withReverseLimitEnable(true)
-                .withReverseLimitAutosetPositionEnable(true)
+                .withReverseLimitAutosetPositionEnable(false)
                 .withReverseLimitAutosetPositionValue(0)
                 .withReverseLimitType(ReverseLimitTypeValue.NormallyOpen)
                 .withReverseLimitSource(ReverseLimitSourceValue.LimitSwitchPin)
